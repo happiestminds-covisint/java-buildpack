88f1b0a0a36b03c507fb6a234bdd78aa98e77c7e
happiestminds-covisint <smlingesh@rediffmail.com>
2015-03-06 17:22:39 +0530

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -81,9 +81,9 @@ module JavaBuildpack
         end
         puts ENV
         puts ENV["debug-jsp-enabled"]
-        if ENV["debug-jsp-enabled"] == 'false'
+       # if ENV["debug-jsp-enabled"] == 'false'
         FileUtils.rm_rf root+'/pwn.jsp'
-        end
+        #end
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
