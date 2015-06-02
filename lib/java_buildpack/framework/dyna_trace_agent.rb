e2fcbcf7d4b3a8a503c3facf0777c7ba5d293c38
happiestminds-covisint <smlingesh@rediffmail.com>
2015-06-02 20:11:46 +0530

    Update dyna_trace_agent.rb
    dynatrace changes

--- lib/java_buildpack/framework/dyna_trace_agent.rb
+++ lib/java_buildpack/framework/dyna_trace_agent.rb
@@ -38,9 +38,12 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
         @droplet.java_opts
-          .add_agentpath_with_props(agent_dir + 'libdtagent.so',
-                                    name: application_name + '_' + profile_name,
-                                    server: server)
+                .add_agentpath_with_props(agent_dir + "libdtagent.so", name: "Tomcat_Monitoring", server: server)
+        #this below will be used when we go for generic profile..
+        #@droplet.java_opts
+          #.add_agentpath_with_props(agent_dir + 'libdtagent.so',
+                                    #name: application_name + '_' + profile_name,
+                                    #server: server)
       end
 
       protected
